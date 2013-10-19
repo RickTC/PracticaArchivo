f21244ac67388e9776a92343b86ed28ba3ddb6ca
Ricardo Tapia <RickTC@localhost.localdomain>
2013-10-19 13:50:16 -0500

    se agrego variables a registra y funcion de leerarchivo en clase funciones.

--- Practica4/Practica4/Funciones.cs
+++ Practica4/Practica4/Funciones.cs
@@ -1,11 +1,59 @@
 using System;
+using System.IO;
+using System.Collections;
 
 namespace Practica4
 {
 	public class Funciones
 	{
+		RegistroPersona registrar = new  RegistroPersona();
+		ArrayList arraywords = new ArrayList();
+		
 		public Funciones ()
 		{
+			this.registrar = new RegistroPersona();
+			this.arraywords = new ArrayList();			
+		}
+		
+		public void leerArchivo()
+		{
+			string uarchivo;
+			
+				try
+				{
+					Console.WriteLine ("Ingrese nombre de archivo o ubicacion");
+					uarchivo = Console.ReadLine();
+				
+					StreamReader leer = new StreamReader (uarchivo);
+					string linea = leer.ReadLine();
+					
+					while(linea != null)
+					{	
+						char [] delimita={'.','-',','};
+						string [] pal = linea.Split(delimita);
+					
+						RegistroPersona reg = new RegistroPersona();
+					
+						reg.id = pal[0];
+						reg.nombre = pal[1];
+						reg.domicilio = pal[2];
+						reg.telefono = pal[3];
+						
+						arraywords.Add(reg);
+						linea = leer.ReadLine();
+							
+					}
+						leer.Close();
+				}
+			    catch(Exception e)
+				{
+					Console.WriteLine ("Exception"+e.Message);				
+				}
+				finally
+				{
+					Console.WriteLine ("Termino lectura");	
+				}
+			
 		}
 	}
 }
--- Practica4/Practica4/RegistraPersona.cs
+++ Practica4/Practica4/RegistraPersona.cs
@@ -4,6 +4,11 @@ namespace Practica4
 {
 	public class RegistraPersona
 	{
+		public string id;
+		public string nombre;
+		public string domicilio;
+		public string telefono;
+		
 		public RegistraPersona ()
 		{
 		}
